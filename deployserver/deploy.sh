445d893ae8f8bafaa7d5c229a9a9eb2bcca56a4b
Stanxxy <stan.x.liu.18@gmail.com>
2021-12-22 12:28:38 -0600

    updated deployment script

--- deployserver/deploy.sh
+++ deployserver/deploy.sh
@@ -1,6 +1,6 @@
 # !/bin/sh
-kill -s 9 $(pgrep webserver)
+kill -s 9 $(pgrep video_server)
 cd ~/newweb/
 git pull git@github.com:Stanxxy/newweb.git master
-cd webserver/
-./webserver &
+cd video_server/
+./video_server &
